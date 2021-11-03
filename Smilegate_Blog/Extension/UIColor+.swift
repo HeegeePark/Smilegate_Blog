7ae3acd9763799ce65823d835c6a7f993bae73f2
heegeepark <phg980513@naver.com>
2021-11-03 18:17:01 +0900

    Feat: Set resources - 아이콘, png, 폰트, 컬러 설정완료

---
+++ Smilegate_Blog/Extension/UIColor+.swift
@@ -0,0 +1,34 @@
+//
+//  UIColor+.swift
+//  Smilegate_Blog
+//
+//  Created by 박희지 on 2021/11/03.
+//
+
+import UIKit
+
+extension UIColor {
+    class var AccentColor: UIColor? {
+        UIColor(named: "AccentColor")
+    }
+    
+    class var LigthGrayColor: UIColor? {
+        UIColor(named: "LigthGrayColor")
+    }
+    
+    class var MidiumGrayColor: UIColor? {
+        UIColor(named: "MidiumGrayColor")
+    }
+    
+    class var TextBlackColor: UIColor? {
+        UIColor(named: "TextBlackColor")
+    }
+    
+    class var WhiteColor: UIColor? {
+        UIColor(named: "WhiteColor")
+    }
+    
+    class var MainBackgruondColor: UIColor? {
+        UIColor(named: "MainBackgruondColor")
+    }
+}
