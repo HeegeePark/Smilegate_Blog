fb5927154e16618eadee61dc8e4024990e088e23
heegeepark <phg980513@naver.com>
2021-11-08 20:13:22 +0900

    Chore: 배제할 규칙 추가 - whitespace, force_cast&unwrapping

---
+++ Smilegate_Blog/Feature/Home/HomeCell.swift
@@ -0,0 +1,8 @@
+//
+//  HomeCell.swift
+//  Smilegate_Blog
+//
+//  Created by 박희지 on 2021/11/07.
+//
+
+import Foundation
---
+++ Smilegate_Blog/Feature/Home/HomeViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  HomeViewModel.swift
+//  Smilegate_Blog
+//
+//  Created by 박희지 on 2021/11/05.
+//
+
+import Foundation
