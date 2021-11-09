1cc4f496c6f0b737699983399751d98ea2cd4ff1
heegeepark <phg980513@naver.com>
2021-11-09 20:17:46 +0900

    Feat: Comment컨뷰 레이아웃 구현 완료

---
+++ Smilegate_Blog/Feature/Comment/CommentCell.swift
@@ -0,0 +1,8 @@
+//
+//  CommentCell.swift
+//  Smilegate_Blog
+//
+//  Created by 박희지 on 2021/11/09.
+//
+
+import Foundation
---
+++ Smilegate_Blog/Feature/Comment/CommentViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CommentViewModel.swift
+//  Smilegate_Blog
+//
+//  Created by 박희지 on 2021/11/09.
+//
+
+import Foundation
