00eba19faa01c0e73b572d64878db1945623d582
heegeepark <phg980513@naver.com>
2021-11-09 17:20:54 +0900

    Feat: Post컨뷰에 해당 셀의 posting 내용 띄우기 완료 - likes 버튼에클릭에 따른 버튼 이미지 바뀜 성공 - 그리고 likes 버튼에 따라 posting 모델의 likes 값 변경 연결가능

--- Smilegate_Blog/Feature/Post/PostViewController.swift
+++ Smilegate_Blog/Feature/Post/PostViewController.swift
@@ -8,15 +8,56 @@
 import UIKit
 
 class PostViewController: UIViewController {
-    @IBOutlet weak var backButton: UIButton!
     let viewModel = PostViewModel()
+    let manager = StorageManager.shared
+    
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var contentLabel: UILabel!
+    @IBOutlet weak var likesButton: UIButton!
+    @IBOutlet weak var likesNumLabel: UILabel!
+    @IBOutlet weak var commentsButton: UIButton!
+    @IBOutlet weak var commentsNumLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateUI()
     }
     @IBAction func backButtonTapped(_ sender: Any) {
         let sb = UIStoryboard(name: "Main", bundle: nil)
         let mainTBC = sb.instantiate("MainTabBarController")
         mainTBC.modalPresentationStyle = .fullScreen
         present(mainTBC, animated: true, completion: nil)
+    }
+    
+    @IBAction func likesButtonTapped(_ sender: Any) {
+        likesButton.isSelected = !likesButton.isSelected
+        viewModel.updateLikes(likesButton.isSelected)
+        likesNumLabel.text = viewModel.posting?.likes
+        
+    }
+    
+    @IBAction func commentsButtonTapped(_ sender: Any) {
+        
+    }
+    
+    func updateUI() {
+        if let postInfo = viewModel.posting {
+            guard postInfo.imageURL != "" else {
+                imgView.image = UIImage(named: "img_default")
+                titleLabel.text = postInfo.title
+                nameLabel.text = postInfo.userName
+                contentLabel.text = postInfo.contents
+                likesNumLabel.text = postInfo.likes
+//                commentsNumLabel.text = postInfo.comments.count
+                return
+            }
+            imgView.image = manager.downloadImage(urlString: postInfo.imageURL)
+            titleLabel.text = postInfo.title
+            nameLabel.text = postInfo.userName
+            contentLabel.text = postInfo.contents
+            likesNumLabel.text = postInfo.likes
+        }
     }
 }
