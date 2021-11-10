76e0ee9dc31458f17a8c6ad023004f1c094fa0fc
heegeepark <phg980513@naver.com>
2021-11-10 19:44:17 +0900

    Feat: Comment뷰모델 구현 완료

--- Smilegate_Blog/Feature/Comment/CommentViewModel.swift
+++ Smilegate_Blog/Feature/Comment/CommentViewModel.swift
@@ -6,3 +6,51 @@
 //
 
 import Foundation
+
+class CommentViewModel {
+    let manager = DatabaseManager.shared
+    var commentsList: [Comment] = [] {
+        didSet {
+            if commentsList.count == 1 { isExist = false }
+        }
+    }
+    var postingId = ""
+    var isExist: Bool = false
+    
+    // timestamp 기준 오름차순 정렬
+    var sortedList: [Comment] {
+        if !isExist { return [] }
+        let sortedList = commentsList[1...commentsList.count-1].sorted { prev, next in
+            return prev.timestamp < next.timestamp
+        }
+        return sortedList
+    }
+    
+    var numOfCommentsList: Int {
+        return sortedList.count
+    }
+    
+    func update(model: [Comment], id: String) {
+        isExist = model.count != 1 ? true: false
+        commentsList = model
+        postingId = id
+        if isExist { Comment.id = Int(commentsList.last!.id)! + 1 }
+    }
+    
+    func comment(at index: Int) -> Comment {
+        return sortedList[index]
+    }
+    
+    func updateComment(comment: Comment) {
+        commentsList += [comment]
+        manager.updateComment(postingId: postingId, comment: comment)
+    }
+    
+    func deleteComment(comment: Comment) {
+        manager.deleteComment(postingId: postingId, id: comment.id)
+        commentsList = commentsList.filter { presentComment in
+            return presentComment.id != comment.id
+        }
+        manager.updateComments(postingId: postingId, comments: commentsList)
+    }
+}
