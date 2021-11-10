6ccae1be7fe08b759e8727c867f00bf849006c2d
heegeepark <phg980513@naver.com>
2021-11-10 19:45:13 +0900

    Feat: CommentCell 구현 완료

--- Smilegate_Blog/Feature/Comment/CommentCell.swift
+++ Smilegate_Blog/Feature/Comment/CommentCell.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+import UIKit
+
+class CommentCell: UITableViewCell {
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var commentLabel: UILabel!
+    
+    // delete button Handler
+    var deleteButtonTapHandler: (() -> Void)?
+    
+    func updateUI(info: Comment) {
+        nameLabel.text = info.userName
+        commentLabel.text = info.content
+    }
+    @IBAction func deleteCommentButtonTapped(_ sender: Any) {
+        // delete comment
+        deleteButtonTapHandler?()
+    }
+}
